7fad8e545b1aed8f4afd4053e961b24060e70cb8
Maksym Pinchuk <maxpinchukrv@gmail.com>
2022-04-03 21:11:59 +0300

    фваывпывапыавп

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,20 @@
 package com.company;
 
 public class Main {
+    public static void main(String[] args) {
 
+        long[][] array = new long[10][5];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 5; j++) {
+                array[i][j] = i * 100 + j;
+            }
+        }
+
+        for (long[] firstLevelArray : array) {
+            for (long item : firstLevelArray) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/com/company/task4/Main.java
+++ src/com/company/task4/Main.java
@@ -1,10 +1,13 @@
 package com.company.task4;
 
-//public class Main {
-//    public static void main(String[] args) {
-//        int[][] score = new int[3][4];
-//        for (int i = 0; i < score.length; i++) {
-//            score[i] = 10 + i;
-//            System.out.println(i);
-//        }
-//    }
+public class Main {
+    public static void main(String[] args) {
+        int[][] score = new int[3][4];
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score.length; j++) {
+                score[i][j] = i * 10 + 1;
+            }
+            System.out.println(i);
+        }
+    }
+}
