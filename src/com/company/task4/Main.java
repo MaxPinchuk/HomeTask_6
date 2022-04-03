5e616fc19ac1bdc183e04ee65a00d27031b9c00c
Maksym Pinchuk <maxpinchukrv@gmail.com>
2022-04-03 20:31:31 +0300

    Initial commit

---
+++ src/com/company/task4/Main.java
@@ -0,0 +1,10 @@
+package com.company.task4;
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int[][] score = new int[3][4];
+//        for (int i = 0; i < score.length; i++) {
+//            score[i] = 10 + i;
+//            System.out.println(i);
+//        }
+//    }
